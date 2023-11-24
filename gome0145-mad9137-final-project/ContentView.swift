d2e1f0af1fd12b5d9406f62fc79130cc50070e3c
M Montserrat Gomez <gome0145@algonquinlive.com>
2023-11-24 14:13:19 -0500

    Initial Commit

---
+++ gome0145-mad9137-final-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gome0145-mad9137-final-project
+//
+//  Created by Montserrat Gomez on 2023-11-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gome0145-mad9137-final-project/gome0145_mad9137_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  gome0145_mad9137_final_projectApp.swift
+//  gome0145-mad9137-final-project
+//
+//  Created by Montserrat Gomez on 2023-11-24.
+//
+
+import SwiftUI
+
+@main
+struct gome0145_mad9137_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
