391ea20a2c6a2a408208262ff060f195b562a0a4
M Montserrat Gomez <gome0145@algonquinlive.com>
2023-11-24 14:43:47 -0500

    Add format to JSON

---
+++ gome0145-mad9137-final-project/Model/BuildingModel.swift
@@ -0,0 +1,13 @@
+//
+//  BuildingModel.swift
+//  gome0145-mad9137-final-project
+//
+//  Created by Montserrat Gomez on 2023-11-24.
+//
+
+import Foundation
+
+struct ModelBuildings: Hashable, Codable, Identifiable {
+	
+	
+}
